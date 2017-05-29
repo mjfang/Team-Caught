98be2123a0360039feb6cb2c0d8f69ff1e0f4f13
Michael Fang <mjfang@stanford.edu>
2017-05-29 01:38:03 -0700

    small fixes

--- components/area/areaController.js
+++ components/area/areaController.js
@@ -3,7 +3,7 @@
 caughtApp.controller('areaController', ['$scope', '$routeParams', '$resource',
   function ($scope, $routeParams, $resource) {
   	$scope.main.title = $routeParams.name;
-  	var worksModel = $resource(main.url_prefix + "/area/:name", {}, {get: {method: 'get', isArray: true}});
+  	var worksModel = $resource("/area/:name", {}, {get: {method: 'get', isArray: true}});
   	$scope.workList = worksModel.get({name:$routeParams.name}, function () {
   		console.log($scope.workList)
   	});
