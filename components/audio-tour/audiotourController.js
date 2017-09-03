7f70cf97bca0775c72fe61b57f327f63423763e0
Michael Fang <mjfang@stanford.edu>
2017-09-02 19:26:47 -0700

    scale factors

--- components/audio-tour/audiotourController.js
+++ components/audio-tour/audiotourController.js
@@ -245,5 +245,31 @@ caughtApp.controller('audiotourController', ['$scope', '$routeParams', '$resourc
 	mapSprite.onload = function() {
 		draw();
 	}
+	(function (logger) {
+    console.old = console.log;
+    console.log = function () {
+        var output = "", arg, i;
+
+        for (i = 0; i < arguments.length; i++) {
+            arg = arguments[i];
+            output += "<span class=\"log-" + (typeof arg) + "\">";
+
+            if (
+                typeof arg === "object" &&
+                typeof JSON === "object" &&
+                typeof JSON.stringify === "function"
+            ) {
+                output += JSON.stringify(arg);   
+            } else {
+                output += arg;   
+            }
+
+            output += "</span>&nbsp;";
+        }
+
+        logger.innerHTML += output + "<br>";
+        console.old.apply(undefined, arguments);
+    };
+})(document.getElementById("log"));
 
   }]);
